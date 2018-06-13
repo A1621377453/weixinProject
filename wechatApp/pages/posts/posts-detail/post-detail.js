c9714b2b2131fa4f85497627480a191b209776dc
A1621377453 <1621377453@qq.com>
2018-06-13 14:31:20 +0800

    添加阅读详情页面收藏功能

--- wechatApp/pages/posts/posts-detail/post-detail.js
+++ wechatApp/pages/posts/posts-detail/post-detail.js
@@ -1,68 +1,122 @@
-var postData = require("../../../data/post-data.js");
+var postsData = require("../../../data/post-data.js");
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    
+
+  },
+
+  onColletionTap: function (event) {
+    // this.getPostsCollectedSyc();
+    this.getPostsCollectedAsy();
+  },
+
+  getPostsCollectedAsy: function () {
+    var that = this;
+    wx.getStorage({
+      key: "posts_collected",
+      success: function (res) {
+        var postsCollected = res.data;
+        var postCollected = postsCollected[that.data.currentPostId];
+        // 收藏变成未收藏，未收藏变成收藏
+        postCollected = !postCollected;
+        postsCollected[that.data.currentPostId] = postCollected;
+        that.showToast(postsCollected, postCollected);
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function (option) {
+    var postId = option.id;
+    //把方法的变量传递到页面Data中，可以在其他方法共享
+    this.data.currentPostId = postId;
+    var postData = postsData.postList[postId];
     this.setData({
-      postData: postData.postList[options.id]
+      postData: postData
     });
+
+    //页面加载的时候查询缓存
+    var postsCollected = wx.getStorageSync('posts_collected')
+    if (postsCollected) {
+      var postCollected = postsCollected[postId];
+      if (postCollected) {
+        this.setData({
+          collected: postCollected
+        })
+      }
+    }
+    else {
+      var postsCollected = {};
+      postsCollected[postId] = false;
+      wx.setStorageSync('posts_collected', postsCollected);
+    }
+  },
+
+  showToast: function (postsCollected, postCollected) {
+    // 更新文章是否的缓存值
+    wx.setStorageSync('posts_collected', postsCollected);
+    // 更新数据绑定变量，从而实现切换图片
+    this.setData({
+      collected: postCollected
+    })
+    wx.showToast({
+      title: postCollected ? "收藏成功" : "取消成功",
+      duration: 1000,
+      icon: "success"
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   }
 })
