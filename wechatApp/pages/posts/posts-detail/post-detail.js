9bb81647135cb332b8edb9a37f540760c2ca21d7
A1621377453 <1621377453@qq.com>
2018-06-13 15:01:19 +0800

    添加分享函数

--- wechatApp/pages/posts/posts-detail/post-detail.js
+++ wechatApp/pages/posts/posts-detail/post-detail.js
@@ -94,6 +94,28 @@ Page({
     })
   },
 
+  //分享函数
+  onShareTap: function (event) {
+    var itemList = [
+      "分享给微信好友",
+      "分享到朋友圈",
+      "分享到QQ",
+      "分享到微博"
+    ];
+    wx.showActionSheet({
+      itemList: itemList,
+      itemColor: "#405f80",
+      success: function (res) {
+        // res.cancel 用户是不是点击了取消按钮
+        // res.tapIndex 数组元素的序号，从0开始
+        wx.showModal({
+          title: "用户 " + itemList[res.tapIndex],
+          content: "用户是否取消？" + res.cancel + "现在无法实现分享功能，什么时候能支持呢"
+        })
+      }
+    })
+  },
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
